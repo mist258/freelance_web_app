4f37f8554966542bb68b5e476c1b2f4701f95faf
Makima3 <natalia.kolchuk@gmail.com>
2024-08-11 14:04:55 +0300

    init commit

--- freelance_web_app/freelancer_profile/apps.py
+++ freelance_web_app/freelancer_profile/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class FreelancerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'freelancer'
+    name = 'freelancer_profile'
