1ea2162f66de8ae321c5bb0cd627a0ce0309eac4
Makima3 <natalia.kolchuk@gmail.com>
2024-09-06 23:47:54 +0300

    making forms for profile edit

--- freelance_web_app/apps/auth_register_user/managers.py
+++ freelance_web_app/apps/auth_register_user/managers.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import BaseUserManager
 
 class CustomUserManager(BaseUserManager):
 
-    def create_user(self, username, first_name, last_name, email=None, password=None, **extra_fields):
+    def create_user(self, username, first_name, last_name, password, email, **extra_fields):
 
         if not email:
             raise ValueError('Users must have an email address')
@@ -21,7 +21,7 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, first_name, last_name, email, password=None, **extra_fields):
+    def create_superuser(self, username, first_name, last_name, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_active', True)
         extra_fields.setdefault('is_superuser', True)
@@ -35,7 +35,6 @@ class CustomUserManager(BaseUserManager):
         if extra_fields['is_active'] is not True:
             raise ValueError('Superuser must have is_active=True.')
 
-        user = self.create_user(username, first_name, last_name, email, password, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
+        return self.create_user(username, first_name, last_name, email, password, **extra_fields)
+
+
