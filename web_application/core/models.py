95c9f813cd5b0d25ae0f6fac6b72993c4ab3b55b
Makima3 <natalia.kolchuk@gmail.com>
2024-10-08 09:44:31 +0300

    add forms

--- web_application/core/models.py
+++ web_application/core/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django_countries.fields import CountryField
+from django.utils.translation import gettext_lazy as _
+
 
 class BaseModel(models.Model):
     profile_photo = ... # todo
-    location = ... # todo
+    location = CountryField(_("Choose your location"))
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
